26f2ab9a3bfa7e8acce5deecec7bfa2ab201dc82
Łukasz Wr <luki5@o2.pl>
2023-06-17 16:16:15 +0200

    generating password

--- passwrordGenerator/passGenerator/views.py
+++ passwrordGenerator/passGenerator/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
-
+import random
+import string
 # Create your views here.
+def generate_password(length, bigLetters=False, numbers=False, specials=False):
+    characters = string.ascii_lowercase # zbiór małych liter
+    if bigLetters:
+        characters += string.ascii_uppercase  # dodawanie dużych liter
+    if numbers:
+        characters += string.digits  # dodawanie liczb
+    if specials:
+        characters += string.punctuation  # dodawanie znaków specialnych
+    password = ''.join(random.choice(characters) for _ in range(length)) # losowanie znaków
+
+    
+    return password
+def home(request):
+    return render(request, 'home.html')
+def password(request):
+    length = int(request.POST.get('length'))
+    big_letters_included = request.POST.get('big')
+    numbers_included = request.POST.get('numbers')
+    specials_included = request.POST.get('specials')
+    password = generate_password(length,big_letters_included, numbers_included,specials_included)
+    return render(request, 'password.html', {'password': password})
