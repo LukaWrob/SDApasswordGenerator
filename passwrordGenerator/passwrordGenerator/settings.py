e54394e5b08152243ebe7d7aa20adf48cc8ed7d6
Łukasz Wr <luki5@o2.pl>
2023-06-17 12:32:47 +0200

    added SECRET_KEY as .env variable

--- passwrordGenerator/passwrordGenerator/settings.py
+++ passwrordGenerator/passwrordGenerator/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u-0m8vo&gd9#q8!i7byq_pr!7)0a682-t^v6z(c&ek+k93f+hh'
+SECRET_KEY = os.environ['SECRET_KEY']
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
